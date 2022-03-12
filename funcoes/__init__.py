f02cb9312f52a7d97545ac741c14f04a9d31cc9c
Leonardo Henrique Mantovani Silva <LeonardoHMS_Git@hotmail.com>
2022-03-12 11:51:30 -0300

    Update Analises
    Foi adicionado uma Analise de dados de arquivo xlsx para determinar as OP's que serão feitas o encerramento

--- funcoes/__init__.py
+++ funcoes/__init__.py
@@ -54,5 +54,7 @@ def text_popup():
 
 def organizar_cabecalho():
     planilha = pd.read_excel(r"C:\Users\Leonardo Mantovani\Desktop\cabecalho.xlsx")
-    planilha['Qtde Falta'] = planilha['Quantidade da ordem (GMEIN)'] - planilha['Qtd.fornecida (GMEIN)']
-    print(planilha['Qtde Falta'])
+    planilha.insert(11, 'Qtde Falta', planilha['Quantidade da ordem (GMEIN)'] - planilha['Qtd.fornecida (GMEIN)'])
+    remove_line = planilha[planilha['Qtde Falta'] > 0].index
+    planilha = planilha.drop(remove_line)
+    planilha.to_excel(r'C:\Users\Leonardo Mantovani\Desktop\novaplanilha.xlsx', index= False)
