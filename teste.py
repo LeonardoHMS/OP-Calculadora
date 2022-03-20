bd291394d031bca3c48ec8f26be048476c0409a5
Leonardo Henrique Mantovani Silva <LeonardoHMS_Git@hotmail.com>
2022-03-20 15:04:33 -0300

    Update teste.py

--- teste.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-planilha = pd.read_excel(r"C:\Users\Leonardo Mantovani\Desktop\cabecalho.xlsx")
-planilha.insert(11, 'Qtde Falta', planilha['Quantidade da ordem (GMEIN)'] - planilha['Qtd.fornecida (GMEIN)'])
-remove_line = planilha[planilha['Qtde Falta'] > 0].index
-planilha = planilha.drop(remove_line)
-planilha.to_excel(r'C:\Users\Leonardo Mantovani\Desktop\novaplanilha.xlsx', index= False)
-print(planilha.info())
