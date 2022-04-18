80414549886126957bac3ec0d7dbe76b90725794
Leonardo Henrique Mantovani Silva <LeonardoHMS_Git@hotmail.com>
2022-04-17 23:16:03 -0300

    Atualizações

--- Opcalculator.py
+++ Opcalculator.py
@@ -1,7 +1,7 @@
 # Programa criado por mim, Leonardo Mantovani
 # Criado como treino de aprendizagem da linguagem python, que se tornou bem útil !!!
 # Utilizo esse programa em meu local de trabalho
-from classes.FrontEnd_GUI import ProgramPainel    
+from classes.frontend_GUI import ProgramPainel
 
 iniciar = ProgramPainel()
 iniciar.start_program()
