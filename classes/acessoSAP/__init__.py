6f667f42cf9412eda7e259d2cb8b285a721a4c36
Leonardo Henrique Mantovani Silva <LeonardoHMS_Git@hotmail.com>
2022-04-20 21:00:41 -0300

    Atualização
    Corrigindo erro de código que fazia o algoritmo falhar na aba de filtros da COOIS.

--- classes/acessoSAP/__init__.py
+++ classes/acessoSAP/__init__.py
@@ -43,23 +43,23 @@ class SapGui(object): # Classe para abrir o sistema SAP
 
     def SapCooisXlsx(self): 
         # Function para gerar planilha, logo faço como classe filha !!!
-        self.session.findById("wnd[0]").sendVKey(2)
-        self.session.findById("wnd[1]/usr/cntlMY_TOOLBAR_CONTAINER/shellcont/shell").pressButton("EXCL")
-        self.session.findById("wnd[1]/usr/cntlOPTION_CONTAINER/shellcont/shell").selectedRows = "0"
-        self.session.findById("wnd[1]/tbar[0]/btn[0]").press()
+        self.session.findById("wnd[0]/usr/tabsTABSTRIP_SELBLOCK/tabpSEL_00/ssub%_SUBSCREEN_SELBLOCK:PPIO_ENTRY:1200/btn%_S_DISPO_%_APP_%-VALU_PUSH").press
+        self.session.findById("wnd[1]/usr/tabsTAB_STRIP/tabpNOSV").Select
+        self.session.findById("wnd[1]/usr/tabsTAB_STRIP/tabpNOSV/ssubSCREEN_HEADER:SAPLALDB:3030/tblSAPLALDBSINGLE_E/ctxtRSCSEL_255-SLOW_E[1,0]").text = "z04"
+        self.session.findById("wnd[0]").sendVKey(8)
         self.session.findById("wnd[0]/usr/tabsTABSTRIP_SELBLOCK/tabpSEL_00/ssub%_SUBSCREEN_SELBLOCK:PPIO_ENTRY:1200/chkP_KZ_E1").Selected = True
         self.session.findById("wnd[0]/usr/tabsTABSTRIP_SELBLOCK/tabpSEL_00/ssub%_SUBSCREEN_SELBLOCK:PPIO_ENTRY:1200/chkP_KZ_E2").Selected = True
-        self.session.findById("wnd[0]/usr/tabsTABSTRIP_SELBLOCK/tabpSEL_00/ssub%_SUBSCREEN_SELBLOCK:PPIO_ENTRY:1200/ctxtS_DISPO-LOW").text = "z04"
         self.session.findById("wnd[0]/usr/tabsTABSTRIP_SELBLOCK/tabpSEL_00/ssub%_SUBSCREEN_SELBLOCK:PPIO_ENTRY:1200/ctxtP_SYST1").text = "ente"
         self.session.findById("wnd[0]/usr/tabsTABSTRIP_SELBLOCK/tabpSEL_00/ssub%_SUBSCREEN_SELBLOCK:PPIO_ENTRY:1200/ctxtP_SYST2").text = "ence"
         self.session.findById("wnd[0]").sendVKey(8)
         time.sleep(10)
-        self.session.findById("wnd[0]/usr/cntlCUSTOM/shellcont/shell/shellcont/shell").currentCellColumn = "MATXT"
-        self.session.findById("wnd[0]/usr/cntlCUSTOM/shellcont/shell/shellcont/shell").contextMenu()
+
+        self.session.findById("wnd[0]/usr/cntlCUSTOM/shellcont/shell/shellcont/shell").pressToolbarButton("&NAVIGATION_PROFILE_TOOLBAR_EXPAND")
+        self.session.findById("wnd[0]/usr/cntlCUSTOM/shellcont/shell/shellcont/shell").pressToolbarContextButton("&MB_EXPORT")
         self.session.findById("wnd[0]/usr/cntlCUSTOM/shellcont/shell/shellcont/shell").selectContextMenuItem("&XXL")
-        self.session.findById("wnd[1]/tbar[0]/btn[0]").press()
-        self.session.findById("wnd[1]/usr/ctxtDY_PATH").text = r"C:\\Users\racao01\Desktop" # Local aonde será salvo o arquivo
-        self.session.findById("wnd[1]/tbar[0]/btn[0]").press()
+        self.session.findById("wnd[1]").sendVKey(0)
+        self.session.findById("wnd[1]/usr/ctxtDY_PATH").text = r"C:\Users\racao01\Desktop" # Local aonde irá salvar a planilha
+        self.session.findById("wnd[1]").sendVKey(0)
         time.sleep(4)
 
 
