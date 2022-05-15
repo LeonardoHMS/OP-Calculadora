4e15d3b60fd6fc92cd0a2733ae754e06d56e9fc2
Leonardo Henrique Mantovani Silva <LeonardoHMS_Git@hotmail.com>
2022-05-15 16:44:50 -0300

    Update

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\calculo_op\venv
+set VIRTUAL_ENV=C:\Users\Leonardo Mantovani\Documents\Github\OP-Calculadora\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
